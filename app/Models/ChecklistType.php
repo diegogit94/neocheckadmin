a9c7cd621251e893738c1451d9a2e61f548d7152
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-03 20:12:23 -0500

    feat: added relationships on ChecklistType model

---
+++ app/Models/ChecklistType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChecklistType extends Model
+{
+    use HasFactory;
+
+    public function certifications()
+    {
+        return $this->hasMany(Certification::class);
+    }
+
+    public function checklist_fields()
+    {
+        return $this->hasMany(ChecklistField::class);
+    }
+}
