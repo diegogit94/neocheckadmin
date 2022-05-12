621af3c97216bbd54f2befc3f566bf2be571cc66
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-12 10:00:06 -0500

    feat: commented not used factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,22 +6,22 @@ use Illuminate\Database\Seeder;
 use App\Models\TimeZone;
 use App\Models\Country;
 use App\Models\User;
-use App\Models\Provider;
-use App\Models\CollectionModel;
-use App\Models\Bank;
-use App\Models\Certification;
-use App\Models\Site;
-use App\Models\ProgrammingLanguage;
-use App\Models\CommercialAgent;
-use App\Models\Merchant;
-use App\Models\ChecklistField;
-use App\Models\ChecklistType;
-use App\Models\Status;
-use App\Models\Evidence;
-use App\Models\Image;
-use App\Models\Project;
-use App\Models\Currency;
-use App\Models\PaymentMethod;
+// use App\Models\Provider;
+// use App\Models\Collection;
+// use App\Models\Bank;
+// use App\Models\Certification;
+// use App\Models\Site;
+// use App\Models\ProgrammingLanguage;
+// use App\Models\CommercialAgent;
+// use App\Models\Merchant;
+// use App\Models\ChecklistField;
+// use App\Models\ChecklistType;
+// use App\Models\Status;
+// use App\Models\Evidence;
+// use App\Models\Image;
+// use App\Models\Project;
+// use App\Models\Currency;
+// use App\Models\PaymentMethod;
 
 class DatabaseSeeder extends Seeder
 {
@@ -36,21 +36,21 @@ class DatabaseSeeder extends Seeder
         Country::factory(50)->create();
         User::factory(99)->create();
         User::factory()->create(['email' => 'mrhyde@admin.com']);
-        Provider::factory(20)->create();
-        CollectionModel::factory(2)->create();
-        Bank::factory(50)->create();
-        Site::factory(300)->create();
-        ProgrammingLanguage::factory(7)->create();
-        CommercialAgent::factory(10)->create();
-        Merchant::factory(80)->create();
-        ChecklistField::factory(200)->create();
-        ChecklistType::factory(7)->create();
-        Status::factory(3)->create();
-        Certification::factory(300)->create(); // Check the relationship between the factories and models
-        Evidence::factory(600)->create();
-        Image::factory(800)->create();
-        Project::factory(300)->create();
-        Currency::factory(20)->create();
-        PaymentMethod::factory(6)->create();
+        // Provider::factory(20)->create();
+        // Collection::factory(2)->create();
+        // Bank::factory(50)->create();
+        // Site::factory(300)->create();
+        // ProgrammingLanguage::factory(7)->create();
+        // CommercialAgent::factory(10)->create();
+        // Merchant::factory(80)->create();
+        // ChecklistField::factory(200)->create();
+        // ChecklistType::factory(7)->create();
+        // Status::factory(3)->create();
+        // Certification::factory(300)->create(); // Check the relationship between the factories and models
+        // Evidence::factory(600)->create();
+        // Image::factory(800)->create();
+        // Project::factory(300)->create();
+        // Currency::factory(20)->create();
+        // PaymentMethod::factory(6)->create();
     }
 }
