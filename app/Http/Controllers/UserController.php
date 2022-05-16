8f160698a14c26e4f631ee57589ddf620cda907c
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-16 14:29:55 -0500

    feat: retrieved user's roles and send it to user view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -50,7 +51,9 @@ class UserController extends Controller
     {
         $user = User::where('id', $id)->get();
 
-        return view('users.user', compact('user'));
+        $roles = Role::all();
+
+        return view('users.user', compact(['user', 'roles']));
     }
 
     /**
