6d485d7657210a85ffe3cfce63ef98c630b38d3d
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-16 15:51:07 -0500

    feat: created search route to users index

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::patch('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::post('/users', [UserController::class, 'search'])->name('users.search');
 
 // Roles
 Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
