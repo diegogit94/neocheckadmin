a3354fa570015f78c812413b06f4fdb44813af07
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-17 15:04:33 -0500

    feat: protected users, roles and permissions buttons in sidebar layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -39,6 +39,7 @@
                                     </a>
                                     {{-- <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">5</div> --}}
                                 </li>
+                                @can('users.index')
                                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
                                     <a href="{{route('users.index')}}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
                                         {{-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-puzzle" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -53,6 +54,8 @@
                                     </a>
                                     {{-- <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">8</div> --}}
                                 </li>
+                                @endcan
+                                @can('roles.index')
                                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
                                     <a href="{{ route('roles.index') }}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
                                         {{-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-compass" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -67,6 +70,7 @@
                                         <span class="text-sm ml-2">Roles y Permisos</span>
                                     </a>
                                 </li>
+                                @endcan
                                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
                                     <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white" >
                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
