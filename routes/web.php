b304f465b69102917a7b3d16d375c82b8fa65e49
Diego Ospina <diego.ospina@evertecinc.com>
2022-05-18 17:28:29 -0500

    feat: commented unnecessary jetstream routes

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 |
 */
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
+// });
 
 // Dashboard
 Route::middleware(['auth'])->group(function () {
